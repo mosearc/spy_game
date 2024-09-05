f4c8c2cee1f014b38e18fd24465d67ec777808d9
mose <mose.arcaro@gmail.com>
2024-09-05 19:48:21 +0200

    footerfix

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,9 +1,10 @@
 <template>
-
+<div id="app">
   <router-view/>
   <footer>
     <p>Made by @mosearc</p>
   </footer>
+</div>
 </template>
 
 <style>
@@ -13,24 +14,15 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+
+
 }
 
-nav {
-  padding: 30px;
-}
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
 footer {
   padding: 20px;
   text-align: center;
-  width: 100%;
+  width: auto;
   background-color: black;
   color: white;
   margin-top: auto;
