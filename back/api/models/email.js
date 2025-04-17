965a4cd070935ffbe651d1b570d4e452ca7c4d4e
mose <mose.arcaro@gmail.com>
2025-04-17 17:10:47 +0200

    expires

--- back/api/models/email.js
+++ back/api/models/email.js
@@ -2,8 +2,13 @@ const mongoose = require('mongoose');
 
 const emailSchema = mongoose.Schema({
     _id: mongoose.Schema.ObjectId,
-    name: {type: String, required: true},
-    code: {type: Number, required: true}
-})
+    name: { type: String, required: true },
+    code: { type: Number, required: true },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        expires: 43200 // Auto-delete after 0.5 day (in seconds)
+    }
+});
 
 module.exports = mongoose.model('Email', emailSchema);
